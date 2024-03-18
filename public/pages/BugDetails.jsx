2cc17b76351da82ad5ca0e28411f15d5b6266777
EdenAlmighty <edengilady1@gmail.com>
2024-03-18 17:32:35 +0200

    Add bug details

--- public/pages/BugDetails.jsx
+++ public/pages/BugDetails.jsx
@@ -25,6 +25,7 @@ export function BugDetails() {
         <h3>Bug Details 🐛</h3>
         <h4>{bug.title}</h4>
         <p>Severity: <span>{bug.severity}</span></p>
+        <pre>Description: <span>{bug.description}</span></pre>
         <Link to="/bug">Back to List</Link>
     </div>
 
